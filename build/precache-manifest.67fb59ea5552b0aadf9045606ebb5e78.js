b2c6b8770bc7fdbe17d6c608e9a6387f3c1962a0
spong-e <alunjones@hotmail.com>
2019-08-12 17:15:45 +0100

    add build folder

---
+++ build/precache-manifest.67fb59ea5552b0aadf9045606ebb5e78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "01a3959a0c865e8647f162e75fd55914",
+    url: "/index.html"
+  },
+  {
+    revision: "3fbe291a1d218fd2a848",
+    url: "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    revision: "4c25d1ef60eb30625546",
+    url: "/static/js/2.e151b3fe.chunk.js"
+  },
+  {
+    revision: "3fbe291a1d218fd2a848",
+    url: "/static/js/main.b3c509eb.chunk.js"
+  },
+  {
+    revision: "c8c3dbd1253827e59a25",
+    url: "/static/js/runtime~main.f87d4d17.js"
+  },
+  {
+    revision: "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    url: "/static/media/logo.5d5d9eef.svg"
+  }
+]);
